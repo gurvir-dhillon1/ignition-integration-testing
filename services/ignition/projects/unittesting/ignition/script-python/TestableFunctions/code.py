5eba3abe25ed38a6beb01217db86be674a5c94ca
gdhillon2 <gman.dhillon@gmail.com>
2025-11-11 02:05:56 -0800

    added test for assertEquals

--- services/ignition/projects/unittesting/ignition/script-python/TestableFunctions/code.py
+++ services/ignition/projects/unittesting/ignition/script-python/TestableFunctions/code.py
@@ -1,2 +1,5 @@
 def getTagValue():
 	return system.tag.readBlocking(['test tag'])[0].value
+	
+def getTag5Value():
+	return system.tag.readBlocking(['always returns 5'])[0].value
--- services/ignition/projects/unittesting/ignition/script-python/unittests/TestTestableFunctions/code.py
+++ services/ignition/projects/unittesting/ignition/script-python/unittests/TestTestableFunctions/code.py
@@ -1,5 +1,7 @@
 import unittest
 import TestableFunctions
+from java.lang import Integer, Double, Boolean, String
+
 class TestTestableFunctions(unittest.TestCase):
 	
 	def testGetTagValue(self):
@@ -8,6 +10,12 @@ class TestTestableFunctions(unittest.TestCase):
 		self.assertIsInstance(value, (int, float, str))
 		print('testGetTagValue passed with:', value)
 		
+	def testTag5Value(self):
+		value = TestableFunctions.getTag5Value()
+		self.assertIsNotNone(value, 'expected a value from tag but got none')
+		self.assertIsInstance(value, (int, float, str))
+		self.assertTrue(Integer(5).equals(value), 'expected tag value 5 but got {}'.format(value))
+
 def run_tests():
 	loader = unittest.TestLoader()
 	suite = loader.loadTestsFromTestCase(TestTestableFunctions)
